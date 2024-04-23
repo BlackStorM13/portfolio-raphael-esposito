c4b64c06fb11623b24b1f02284742faae504adfa
BlackStorM13 <77778690+BlackStorM13@users.noreply.github.com>
2024-04-23 16:17:14 +0200

    Ajout code

---
+++ Portfolio 1/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./public/index.html"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
